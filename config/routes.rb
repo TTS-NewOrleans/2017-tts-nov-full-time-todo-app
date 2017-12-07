965769ded4a0044f2d5586fd670ce8d82a671199
Ryan Hageman <improv42@gmail.com>
2017-12-07 11:15:58 -0600

    Associate tasks with a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', as: :signout
+  get '/signin' => 'sessions#new', as: :signin
 
   resources :user_tasks
 
