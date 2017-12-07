485d02a91ac75396bfc60b99e66a9cc8a7d37e37
Ryan Hageman <improv42@gmail.com>
2017-12-07 09:26:09 -0600

    Setup user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206145302) do
+ActiveRecord::Schema.define(version: 20171207151101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20171206145302) do
   create_table "user_tasks", force: :cascade do |t|
     t.string "description"
     t.date "due"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
